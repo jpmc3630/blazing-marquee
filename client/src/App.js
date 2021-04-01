54c5e9012533b0aa6da3e6439b381d5eaecd961a
James McGlone <jamespmcglone@gmail.com>
2021-04-02 06:51:55 +0800

    Add files

---
+++ client/src/App.js
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+
+import io from "socket.io-client";
+import "./App.css"
+
+let socket;
+if (process.env.NODE_ENV === 'development') {
+  socket = io(`http://localhost:3001/`);
+} else {
+  socket = io();
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      response: false,
+      socket: false,
+      currentRoom: null,
+      roomToJoin: null,
+      username: null,
+      color: '#7c1313',
+      statusArr: [],
+      conToHost: false,
+      conToServer: false,
+      status: 'lobby',
+      statusMessage: 'Searching for server...',
+      answers: [],
+      message: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAnswer = this.handleAnswer.bind(this)
+  }
+
+  componentDidMount() {
+    
+    socket.on('connect', (socket) => {
+      this.setState({
+        conToServer: true,
+        status: 'lobby',
+        statusMessage: 'Connected to server.'
+      })
+    })
+
+    // can't get this to recieve for some reason
+    socket.on('status', (data) => {
+      this.setState({
+        status: data.status,
+        statusMessage: data.statusMessage
+      })
+
+      if (data.status == 'question') {
+        this.setState({
+          answers: data.answers
+        })
+      }
+    })
+
+    socket.on('disconnectedHost', (data) => {
+      this.setState({ statusArr: [], conToHost: false, status: 'The host has stopped hosting!' });
+    })
+   
+  
+  }
+
+  componentWillUnmount() {
+    this.props.socket.emit('removeUser', {room: this.state.currentRoom, msg: `removeUser`});
+    this.setState({ statusArr: [], conToHost: false });
+  }
+
+  handleChange(event) {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+  }
+
+  
+  handleSubmit(event) {
+      event.preventDefault()
+      if(this.state.conToServer) {
+        if(this.state.message.trim() != '') {
+        socket.emit('submitMessage', {
+          // roomName: this.state.roomToJoin, 
+          message: this.state.message,
+          color: this.hexTorgb(this.state.color)
+        });
+      } else {
+        alert('Spit it out')
+      }
+      } else {
+        alert('Reception is a bit shit, mate')
+      }
+  }
+
+  handleAnswer(event) {
+    event.preventDefault()
+      socket.emit('playerAnswer', {
+        roomName: this.state.roomToJoin, 
+        username: this.state.username,
+        answer: event.target.dataset.answer
+      });
+
+      this.setState({
+        status: 'wait',
+        statusMessage: 'Your answer has been recorded!'
+      })
+  }
+  
+   hexTorgb(hex) {
+    return ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
+  }
+  
+  render() {
+    
+    return (
+      
+      <div className="App">
+        <div className="container">
+
+
+        {(() => {
+        switch (this.state.status) {
+
+          case 'lobby':
+            return (
+      
+              <div>
+              <h4>Message Board!</h4>
+      
+                  <form className="form-horizontal">
+                    <div className="form-group">
+                        <div className="col-1 col-ml-auto">
+                            <label className="form-label" htmlFor="username">{this.state.statusMessage}</label>
+                        </div>
+                        <div className="col-3 col-mr-auto">
+                          <div className="col-1 col-ml-auto">
+                            <label className="form-label" htmlFor="message">Message:</label>
+                        </div>
+                            <input className="form-input"
+                                type="text"
+                                id="message"
+                                name="message"
+                                placeholder="Message"
+                                value={this.state.message}
+                                onChange={this.handleChange}
+                            />
+                          <br></br>
+                          <br></br>
+                          <label htmlFor="favcolor">Color:</label>
+                          <br></br>
+                        <input type="color" id="favcolor" className="colorPicker" name="color" onChange={this.handleChange} value={this.state.color}></input>
+                        </div>
+                    </div>
+                    
+                    <div className="form-group ">
+                        <div className="col-7"></div>
+                        <br></br>
+                        <button
+                            className="btn synthToolButton col-1 col-mr-auto"
+                            onClick={this.handleSubmit}
+                            type="submit">Send message!</button>
+                    </div>
+                  </form>
+                </div>
+          )
+
+    
+
+
+
+
+          case 'wait':
+            return (
+              <p>{this.state.statusMessage}</p>
+          )
+
+
+
+
+          default:
+            return (
+              <p>Hitting default over here...
+                <br></br>
+                Status: {this.state.status}
+                <br></br>
+                StatusMessage: {this.state.statusMessage}
+            </p>
+            )
+        }
+      })()}
+
+          
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const axios = require('axios');
+
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+
+
+
+// set up socket.io from our express connection
+var io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+let screenObject = {};
+
+// handle incoming connections from clients
+io.sockets.on('connection', function(socket) {
+  
+  console.log(socket.id)
+  // sorting out room creation for hosts ... 
+  // once a client has connected, we expect to get a ping from them saying what room they want to join
+  socket.on('screenConnect', function(data) {
+      // if (data === 'create') {
+      //     let roomName = 'the-room'
+        
+      //     socket.join(roomName)
+      //     console.log('room message recieved:' + roomName)
+          
+          screenObject = {
+            socket: socket.id,
+          }
+
+          // io.sockets.emit('getHosts', hostsArr)
+          // io.sockets.in(roomName).emit('room', { roomName })
+          console.log('Screen has connected')
+          console.log('Screen object is:')
+          console.log(JSON.stringify(screenObject, null, 4))
+      
+  });
+
+  // join room, for joiners
+  socket.on('submitMessage', function({ message, color }) {
+    
+    // console.log('client join room message recieved:' + roomName)
+    console.log('message: ' + message)
+    console.log('color: ' + color)
+
+    socket.to(screenObject.socket).emit('startMessage', {
+      messsage: message,
+      color: color
+    })
+    
+  });
+
+
+
+// socket.on('removeHost', function() {
+//   console.log('Screen removeHost');
+// });
+
+
+  // on DISCONNECT or CLOSE TAB remove host from hosts list
+  socket.on('disconnect', function(reason) {
+    
+    screenObject = {}
+
+      console.log('Screen object after disconnect:')
+      console.log(JSON.stringify(screenObject, null, 4))
+
+  });
+
+
+});
+
