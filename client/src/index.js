ae96b68758b56f3618bb2b822a631b2d0c14e968
James McGlone <jamespmcglone@gmail.com>
2023-03-26 01:04:17 +0800

    Update service worker implementation

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+serviceWorker.register({
+  onUpdate: registration => {
+    alert('New version available!  Ready to update?');
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  }
+});
