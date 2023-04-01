18bf360fa366bcdca8365f87cac69b65b91b51c5
James McGlone <jamespmcglone@gmail.com>
2023-04-01 14:41:14 +0800

    Experiment

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,18 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+window.onfocus = () => {
+  console.log('The user is back to the page!')
+  // When the user comes back to the page, we want to make sure that the
+  // service worker is activated. This is because the service worker will
+  // stop running when the user closes the tab.
+  // We can use the `clients.claim()` method to activate the service worker
+  // immediately.
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage({ type: 'CLAIM_CLIENTS' });
+  }
+};
+
 serviceWorker.register({
   onUpdate: async registration => {
     // We want to run this code only if we detect a new service worker is
