f4b8af3333660b49b3d79752a7fbefa24f58648a
James McGlone <jamespmcglone@gmail.com>
2023-04-01 13:51:26 +0800

    SW await unregister in onUpdate

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,17 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.register({
-  onUpdate: registration => {
-    // const ok = confirm('New version available. Press ok to reload!')
-    // if (ok) {
-      console.log('Reloading SW')
-      window.location.reload()
-      if (registration && registration.waiting) {
-        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-      }
-      Promise.resolve().then(() => { window.location.reload(true); });
-    // }
-  }
+  onUpdate: async registration => {
+    // We want to run this code only if we detect a new service worker is
+    // waiting to be activated.
+    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      // Makes Workbox call skipWaiting()
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload();
+    }
+  },
 });
