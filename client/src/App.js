fb5a825c674023bf093164c71993fb765ee66178
James McGlone <jamespmcglone@gmail.com>
2023-04-06 18:25:24 +0800

    Bump speed to 1.3

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       color: '#7c1313',
       colorOutline: '#7c1313',
       bgColor: '#000000',
-      speed: "1.2",
+      speed: "1.3",
       spacing: "1",
       statusArr: [],
       conToHost: false,
